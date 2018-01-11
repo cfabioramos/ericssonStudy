43f106058407832acfd1f5ca2ec7acf180956857
Carlos Fábio Ramos Conceição <cfabioramos@gmail.com>
2018-01-11 14:05:32 -0200

    task list

--- restEasyJpa/src/main/java/estudoCaelum/restEasyJpa/model/PessoaModel.java
+++ restEasyJpa/src/main/java/estudoCaelum/restEasyJpa/model/PessoaModel.java
@@ -1,5 +1,6 @@
 package estudoCaelum.restEasyJpa.model;
 
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import org.hibernate.validator.constraints.NotEmpty;
@@ -8,9 +9,11 @@ import org.hibernate.validator.constraints.NotEmpty;
 public class PessoaModel {
 	
 	private Long id;
-	@NotEmpty
+	@NotEmpty(message="O nome deve ser informado.")
+	@Size(min=5, max=50, message="O nome deve conter entre 5 e 50 caracteres.")
 	private String nome;
 	@NotEmpty
+	@Size(min=5, max=50, message="O sobrenome deve conter entre 5 e 50 caracteres.")
 	private String sobreNome;
 
 	public Long getId() {
